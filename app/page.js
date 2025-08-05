7389d03b7c08c225e94df601b2c9d1065043245e
Bilal <bilaltoor786786@gmail.com>
2025-08-05 01:31:59 -0700

    Update CI/CD workflow and fix homepage title

--- app/page.js
+++ app/page.js
@@ -4,7 +4,7 @@ import TodoList from '../components/TodoList';
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24 bg-gray-900 text-white">
-      <h1 className="text-4xl font-bold mb-8">Todo List hey latest code a is there</h1>
+      <h1 className="text-4xl font-bold mb-8">bilaltoor cicd is working fine </h1>
       <div className="w-full max-w-md">
         <Calculator />
         <TodoList />
